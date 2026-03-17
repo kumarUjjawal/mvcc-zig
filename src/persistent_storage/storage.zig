3b84627e5f0b0b006f08281a71813ac6ff60336c
Kumar Ujjawal <ujjawalpathak6@gmail.com>
2026-03-17 22:49:01 +0530

    stub for storage

--- src/persistent_storage/storage.zig
+++ src/persistent_storage/storage.zig
@@ -1,1 +1,29 @@
-const Storage = struct {};
+const std = @import("std");
+
+pub fn assertStorage(comptime T: type) type {
+    comptime {
+        if (!@hasDecl(T, "appendLogRecord")) {
+            @compileError(@typeName(T) ++ " must declare appendLogRecord(self: *T, record: anytype) !void");
+        }
+
+        if (!@hasDecl(T, "deinit")) {
+            @compileError(@typeName(T) ++ " must declare deinit(self: *T, allocator: std.mem.Allocator) void");
+        }
+    }
+}
+
+pub const NullStorage = struct {
+    pub fn init() NullStorage {
+        retun .{};
+    }
+
+    pub fn deinit(self: *NullStorage, allocator: std.mem.Allocator) void {
+        _ = self;
+        _ = allocator;
+    }
+
+    pub fn appendLogRecord(self: *NullStorage, record: anytpe) !void {
+        _ = self;
+        _ = record;
+    }
+};
